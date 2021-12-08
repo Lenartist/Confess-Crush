bee3c5db0595dcae03e3a18dac90264d0b3e9142
Lenartist <95775155+Lenartist@users.noreply.github.com>
2021-12-09 02:39:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    introTitle: 'Hi Ren',
+    introDesc: `To get your birthday gift, you have to answer the next question with honesty,
+    btnIntro: 'Okie',
+    title: 'Do you love me',
+    desc: 'Remember, you have to be HONEST',
+    btnYes: 'Yush',
+    btnNo: 'Hell no',
+    question: 'Why do you love me so?',
+    btnReply: 'You must be sure when you send this answer',
+    reply: 'Cause you are your sweet candy xD',
+    mess: 'I knew it, love you too dear ^^',
+    messDesc: 'You are getting close to your treasure, inbox me for the next step',
+    btnAccept: 'Okie',
+   
 }
